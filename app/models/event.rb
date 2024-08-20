c35f9dc95881ebe5766978cd33e747a5c6704256
Sebastien Teston <teston.sebastien@gmail.com>
2024-08-20 11:37:40 +0200

    add associations and validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,23 @@
 class Event < ApplicationRecord
+  #associations
   belongs_to :user
+  has_many :follows, dependent: :destroy
+  has_many :subs, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  # has_many :users_follow, through: :follow, source: :user
+  # has_many :users_sub, through: :sub, source: :user
+  # has_many :users_review, through: :revienw, source: :user
+
+  #validations
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :user_id, presence: true
+  validates :price, presence: true
+  validates :category, presence: true
+  validates :theme, presence: true
+  validates :participants_min, presence: true
+  validates :participants_max, presence: true
+  validates :date_sub, presence: true
+  validates :date_event, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
+  #associations
   belongs_to :user
   belongs_to :event
+
+  #validation
+  validates :user_id, presence: true
+  validates :event_id, presence: true
+  validates :comment, presence: true
+  validates :rating, presence: true
 end
