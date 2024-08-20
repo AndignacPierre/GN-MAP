1a057953c58b55c58c945ee00b9753f690f95e3b
Sylvain Bertrand <sylv.bertrand@orange.fr>
2024-08-20 17:05:06 +0200

    index majeur 2

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,4 +17,7 @@ class Event < ApplicationRecord
   validates :category, presence: true
   validates :theme, presence: true
   validates :date_event, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
