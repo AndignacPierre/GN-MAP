56228d343b7666d528117eafb330ffa824530f39
Sebastien Teston <teston.sebastien@gmail.com>
2024-08-29 11:11:40 +0200

    messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
+
+  def index
+    @received_messages = current_user.received_messages
+    @sent_messages = current_user.sent_messages
+  end
+
+
   def show
     @message = Message.find(params[:id])
     @sender = @message.sender
@@ -9,6 +16,16 @@ class MessagesController < ApplicationController
     @message = Message.new
     @users = User.where.not(id: current_user.id)
   end
+
+  def create
+    @message = current_user.sent_messages.build(message_params)
+    if @message.save
+      redirect_to messages_path, notice: 'Message sent!'
+    else
+      render :new
+    end
+  end
+
 
   private
 
